37ea670022039f5c06ccd687eb9557bee22a3089
Dr Pawel Orzechowski <24914654+drpawelo@users.noreply.github.com>
2026-05-22 14:19:40 +0100

    wip

--- _theme/filters/callout-for-docx.lua
+++ _theme/filters/callout-for-docx.lua
@@ -1,12 +1,48 @@
-function Div(el)
-  if el.classes:includes("callout") and el.classes:includes("warning") then
-    -- Replace with a simple paragraph with a custom style
-    return pandoc.Para{
-      -- todo, figure out how callouts hsould look and adjust this
-      pandoc.Strong("NOTE: "),
-      pandoc.Str(el.content[1].text)
-    }
-  end
+local style_map = {
+  ["callout-note"] = "BoxHeaderInfo",
+  ["callout-warning"] = "BoxHeaderWarning",
+  ["callout-important"] = "BoxHeaderImportant",
+  ["callout-tip"] = "BoxHeaderTip",
+  ["callout-caution"] = "BoxHeaderCaution"
+}
+
+local calloutBody = "BoxBody"
+
+local title_map = {
+  ["callout-note"] = "Info",
+  ["callout-warning"] = "Warning",
+  ["callout-important"] = "Important",
+  ["callout-tip"] = "Tip",
+  ["callout-caution"] = "Caution"
+}
+
+local function make_styled_para(text, style)
+  return pandoc.Para(
+    { pandoc.Str(text) },
+    { ["custom-style"] = style }
+  )
 end
 
--- if this is callout, represent as another style
+function Div(el)
+  for class, style in pairs(style_map) do
+    if el.classes:includes(class) then
+
+      local title = title_map[class]
+
+      -- use explicit callout title if present
+      if el.attributes.title then
+        title = el.attributes.title
+      end
+
+      table.insert(
+        el.content,
+        1,
+        make_styled_para(title, style)
+      )
+
+      return el
+    end
+  end
+
+  return el
+end
